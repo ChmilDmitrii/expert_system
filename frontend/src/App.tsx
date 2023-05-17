d102415a84d5e8d7d207d319a9978f5d5564eaf4
Albert <a.teregulov@bet2u.com>
2023-05-17 11:02:44 +0300

    add componet (appbar) and change tsconfig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { ButtonGroup, Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Box } from '@mui/material'
+import SideBar from './component/SideBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className='App'>
-        <Link to={`definition-disease`}><Button variant='contained'>Определение болезни</Button></Link>
-        <Link to={`knowledge-base`}><Button variant='contained'>База знаний</Button></Link>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Box className="App" sx={{ flexGrow: 1 }}>
+                <SideBar />
+            </Box>
+        </>
+    )
 }
 
 export default App
--- frontend/src/pages/DefinitionDisease/ui.tsx
+++ frontend/src/pages/DefinitionDisease/ui.tsx
@@ -1,114 +1,171 @@
-import { TableRow, TableCell, Checkbox, IconButton, Collapse, Box, Typography, TableContainer, Paper, Table, TableBody, Button } from "@mui/material";
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import axios from "axios"
-import React from "react"
+import {
+    TableRow,
+    TableCell,
+    Checkbox,
+    IconButton,
+    Collapse,
+    Box,
+    TableContainer,
+    Paper,
+    Table,
+    TableBody,
+    Button,
+} from '@mui/material'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import axios from 'axios'
+import React from 'react'
+import SideBar from '../../component/SideBar'
 import './style.css'
 
-function Row(props: { row: any, update: any}) {
-    const { row, update } = props;
-    const [open, setOpen] = React.useState(false);
-
+function Row(props: { row: any; update: any }) {
+    const { row, update } = props
+    const [open, setOpen] = React.useState(false)
 
     return (
         <React.Fragment>
-        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-          <TableCell>
-            <IconButton
-              aria-label="expand row"
-              size="small"
-              onClick={() => setOpen(!open)}
-            >
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {row.title}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            <Checkbox onChange={() => update(row.code)} />
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box sx={{ margin: 1 }}>
-                <div>
-                    <div>Описание: {row.description}</div>
-                </div>
-              </Box>
-            </Collapse>
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? (
+                            <KeyboardArrowUpIcon />
+                        ) : (
+                            <KeyboardArrowDownIcon />
+                        )}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {row.title}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    <Checkbox onChange={() => update(row.code)} />
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell
+                    style={{ paddingBottom: 0, paddingTop: 0 }}
+                    colSpan={6}
+                >
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{ margin: 1 }}>
+                            <div>
+                                <div>Описание: {row.description}</div>
+                            </div>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
     )
 }
-
 
 export const Definition = () => {
     const [rows, setRows] = React.useState<any>(null)
     const [answer, setAnswer] = React.useState<any>({})
-    
+
     const handleClick = async () => {
         let query = {}
-        for(let i = 0; i < rows.length; i++) {
-            query = {...query, [rows[i].code]: rows[i]}
+        for (let i = 0; i < rows.length; i++) {
+            query = { ...query, [rows[i].code]: rows[i] }
         }
-        const data:any = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/disease/`, {
-            ...query
-        })   
+        const data: any = await axios.post(
+            `${import.meta.env.VITE_BASE_URL}/api/disease/`,
+            {
+                ...query,
+            }
+        )
         setAnswer(data.data)
     }
-    React.useEffect(()=> {
+    React.useEffect(() => {
         const deirea = async () => {
-            const data:any = (await axios.get(`${import.meta.env.VITE_BASE_URL}/api/symptom/`)).data
+            const data: any = (
+                await axios.get(`${import.meta.env.VITE_BASE_URL}/api/symptom/`)
+            ).data
             const arr = []
-            for(var elem in data) {
-                arr.push({...data[elem], code: elem})
+            for (var elem in data) {
+                arr.push({ ...data[elem], code: elem })
             }
             setRows(arr)
         }
 
         deirea()
-    },[])
-    
-    const update = (code:string) => {
-        const arr = rows.map((item:any) => item.code === code ? {...item, flag: !item.flag} : item)
+    }, [])
+
+    const update = (code: string) => {
+        const arr = rows.map((item: any) =>
+            item.code === code ? { ...item, flag: !item.flag } : item
+        )
         setRows(arr)
     }
 
     return (
-        <div className="bg">
-           {rows ? <TableContainer style={{background: "#fff"}} component={Paper}>
-            <div>
-                <div style={{background: '#000', height: '50px', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
-                    <div style={{ color: '#fff',border: 'none'}}>Симптомы</div>
-                </div>
-            </div>
-            <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-            
-                <TableBody>
-                {rows.map((row:any) => (
-                    <Row key={row.title} row={row} update={update} />
-                ))}
-                </TableBody>
-            </Table>
-            </TableContainer> : 'load'}
-            <div className="rightColumn">
-                <div className="fixed">
-                    <div className="responser">
-                        <div>Болезнь: {answer.title}</div>
-                        <div>Возраст от: {answer.age_from}</div>
-                        <div>Возраст до: {answer.age_to}</div>
-                        <div>Описание: {answer.description}</div>
-                        <div>Муж. пол: {answer.is_female}</div>
-                        <div>Жен. пол: {answer.is_male}</div>
-                        <div>Профилактика: {answer.prophylaxis}</div>
-                        <div>Лечение: {answer.treatment}</div>
+        <Box className="App" sx={{ flexGrow: 1 }}>
+            <SideBar />
+            <div className="bg" style={{ marginTop: '1em' }}>
+                {rows ? (
+                    <TableContainer
+                        style={{ background: '#fff' }}
+                        component={Paper}
+                    >
+                        <div>
+                            <div
+                                style={{
+                                    background: '#000',
+                                    height: '50px',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    display: 'flex',
+                                }}
+                            >
+                                <div style={{ color: '#fff', border: 'none' }}>
+                                    Симптомы
+                                </div>
+                            </div>
+                        </div>
+                        <Table
+                            sx={{ minWidth: 500 }}
+                            aria-label="custom pagination table"
+                        >
+                            <TableBody>
+                                {rows.map((row: any) => (
+                                    <Row
+                                        key={row.title}
+                                        row={row}
+                                        update={update}
+                                    />
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                ) : (
+                    'load'
+                )}
+                <div className="rightColumn">
+                    <div className="fixed">
+                        <div className="responser">
+                            <div>Болезнь: {answer.title}</div>
+                            <div>Возраст от: {answer.age_from}</div>
+                            <div>Возраст до: {answer.age_to}</div>
+                            <div>Описание: {answer.description}</div>
+                            <div>Муж. пол: {answer.is_female}</div>
+                            <div>Жен. пол: {answer.is_male}</div>
+                            <div>Профилактика: {answer.prophylaxis}</div>
+                            <div>Лечение: {answer.treatment}</div>
+                        </div>
+                        <Button
+                            variant="contained"
+                            className="btn"
+                            onClick={handleClick}
+                        >
+                            Отправить
+                        </Button>
                     </div>
-                    <Button variant="contained" className="btn" onClick={handleClick}>Отправить</Button>
                 </div>
             </div>
-        </div>
+        </Box>
     )
 }
